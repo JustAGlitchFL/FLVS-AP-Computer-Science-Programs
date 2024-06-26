80dbcb67bddd53c5e6b02885a12e563b7427d0b6
Aidan Procopio <97538549+JustAGlitchFL@users.noreply.github.com>
2024-06-26 09:10:29 -0400

    starting 2.10

---
+++ M2/L10/smartphonepurchases.java
@@ -0,0 +1,20 @@
+/**
+This program demonstrates a simple sales system, inputting data and returning a receipt to the user
+
+@author Aidan Procopio 
+@version 06/26/24
+
+ */
+import java.util.Scanner;
+
+public class smartphonepurchases {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Please enter your first and last name: ")
+        String firstname = in.next();
+        String lastname = in.nextLine();
+        Char firstinitial = (char) firstname.substring(0,1);
+        System.out.println(firstinitial)
+    }
+}
