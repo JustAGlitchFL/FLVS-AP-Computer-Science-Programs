d30ec0d6cdb7424fc2689a67c43e15efa5ace398
Aidan Procopio <97538549+JustAGlitchFL@users.noreply.github.com>
2024-06-27 17:20:46 +0000

    stuff

--- M2/L6/test.java
+++ M2/L6/test.java
@@ -1,13 +1,15 @@
-public class test {    
+import java.util.Scanner;
+public class test {
     public static void main(String[] args) {
+        double heightIn = 67.34;
+        double heightCm = 2.54 / heightIn;
+        double weightLb = 150.0;
+        double weightKg = 0.453592 * weightLb;
+        int age = 25;
+        double s = 5.0;
 
-        
-        double fact1 = 1 / 2;
-
-        double fact2 = 3 * 4;
-        
-        double product = fact1 * fact2;
-        
-        System.out.println(product);
+        if(true) {
+            double bMR = (10.0 * weightKg) + (6.25 * heightCm) - (5.0 * age) + s;
+        }
     }
 }
