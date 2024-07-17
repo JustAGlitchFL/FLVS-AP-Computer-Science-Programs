533f1c631af71651e6474d551b9fedb8e66fbb45
JustAGlitchFL <aidanprocopio@gmail.com>
2024-07-17 01:25:13 -0400

    kjgfudt

--- M4/L1/test.java
+++ M4/L1/test.java
@@ -2,15 +2,10 @@ package M4.L1;
 
 public class test {
     public static void main(String[] args) {
-        int x = 0;
-        int lower = 10;
-        int upper = 20;
-        while(x < 20)
-        {
-          //print a random number with the lower limit being inclusive and the upper limit being exclusive
-          System.out.println((int)(Math.random() * (upper - lower) + lower));
-          x ++;
-        }
-        
+        String phone ="1234567890";
+        System.out.println(phone.substring(6,9));
+        System.out.println(phone.substring(6));
+        System.out.println(phone.substring(6,10));
+
     }
 }
