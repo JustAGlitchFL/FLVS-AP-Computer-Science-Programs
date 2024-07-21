33e3e23dc558ba4146410ede7df4a7f642c32cb1
JustAGlitchFL <97538549+JustAGlitchFL@users.noreply.github.com>
2024-07-21 00:58:52 -0400

    stuff

---
+++ M4/L6/SecretPasscode.java
@@ -0,0 +1,79 @@
+package M4.L6;
+/**
+ * Randomly generate passwords.
+ *
+ * @author Aidan Procopio
+ * @version 07/18/2024
+ * 
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.File;
+import java.util.Scanner;
+
+public class SecretPasscode
+{
+        public static void main(String [] args) throws IOException
+        {
+                String userChoice = "Y";
+                int pwLength = 0;
+                int randNum = 0; 
+                String password = "";
+
+                // initialize scanner and print writer
+                Scanner in = new Scanner(System.in);
+                PrintWriter outFile = new PrintWriter(new File("passwords.txt"));
+
+                System.out.println("**********************************************************");
+                System.out.println("Welcome to a simple password generator. \nThe password will include lowercase, uppercase, & numbers.");
+                
+                // the user wants to continue
+                System.out.println("Press enter to start.");
+                in.nextLine();
+                while (userChoice.equalsIgnoreCase("Y")) {
+                        while (pwLength < 6) {        
+                                // ask for password length 
+                                System.out.print("Enter the length of the password (at least 6): ");
+                                pwLength = in.nextInt();
+                                if (pwLength < 6) {
+                                System.out.println("Password length must be at least 6 characters.");
+                                }
+                                in.nextLine();
+                        }
+                                
+                        // generate a random character in the correct character set
+                        while (password.length() < pwLength) {
+                                randNum = (int)(Math.random() * 3);
+                                if (randNum == 0) {
+                                        password += (char)(Math.random() * 26 + 65); // uppercase
+                                } else if (randNum == 1) {
+                                        password += (char)(Math.random() * 26 + 97); // lowercase
+                                } else {
+                                        password += (char)(Math.random() * 10 + 48); // numbers
+                                }
+                                
+                        }
+
+                        // write the password to the file
+                        outFile.println(password);
+                        // clear the password
+                        password = "";
+                        // ask the user if they want to continue
+                        System.out.print("Do you want to generate another password? (Y/N): ");
+                        userChoice = in.nextLine();
+                        pwLength = 0;
+                }
+                // when the user is done, close the text file so that it can be re-opened and read
+                outFile.close();
+
+                // read passwords back from text file and display to the screen
+
+                Scanner inFile = new Scanner(new File("passwords.txt"));
+                System.out.println("Here are the passwords you generated:");
+                while (inFile.hasNext()) {
+                        System.out.println(inFile.nextLine());
+                }
+
+        }//end main
+}//end class
+
--- M4/L6/printwriterdemo.java
+++
@@ -1,9 +0,0 @@
-package M4.L6;
-import java.io.PrintWriter;
-import java.io.File;
-import java.io.IOException;
-public class printwriterdemo {
-    public static void main(String[] args) {
-        
-    }
-}
