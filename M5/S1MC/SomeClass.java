dcc9486851c6626b2a9edee922aefa1c2ce8afc9
JustAGlitchFL <aidanprocopio@gmail.com>
2024-07-23 23:01:22 -0400

    stuff

---
+++ M5/S1MC/SomeClass.java
@@ -0,0 +1,22 @@
+package M5.S1MC;
+
+public class SomeClass
+{
+   private int x, y;
+
+   public SomeClass (int xValue, int yValue)
+   {
+     x = xValue;
+     y = yValue;
+   }            
+   public void m1()
+   {
+     x = 30;
+     System.out.print((y + 1) + " ");
+   }
+   public void m2()
+   {
+     m1();
+     System.out.print(x + " ");
+   }
+}
---
+++ M5/S1MC/SomeTester.java
@@ -0,0 +1,13 @@
+package M5.S1MC;
+
+public class SomeTester
+{
+   public static void main (String[] args)
+   {
+     int x = 10;
+     int y = 20;
+     SomeClass z = new SomeClass(x, y);
+     z.m2();
+     z.m1();
+   }
+}
