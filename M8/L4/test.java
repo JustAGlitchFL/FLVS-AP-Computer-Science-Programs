1ad7e7f8d6200bf0b2e137089b9e474fff3ecd46
JustAGlitchFL <aidanprocopio@gmail.com>
2024-10-25 02:38:44 -0400

    8.4

---
+++ M8/L4/test.java
@@ -0,0 +1,9 @@
+package M8.L4;
+
+public class test {
+    public static void main(String[] args) {
+        double[][] gpas = {{4.2, 3.0, 7.2, 1.8},{2.2, 3.5, 3.0, 6.4},{4.3, 3.9, 3.2, 2.9}} ;
+        System.out.println(gpas[2].length);
+        
+    }
+}
